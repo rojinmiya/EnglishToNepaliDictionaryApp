f7085e1666a7555012652032e168a3ed4a384840
Softwarica College Lab <softwarica@softwarica.com>
2019-04-25 10:27:25 +0545

    adding add word activity and designing its front end

---
+++ app/src/main/java/com/dictionaryapp/AddWordActivity.java
@@ -0,0 +1,13 @@
+package com.dictionaryapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddWordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_word);
+    }
+}
